2f83e4cf11c56554c5c131c6dffc82edcf975d23
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-10-02 22:42:12 -0300

    feat: add configs in project

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,25 @@
+export default {
+    colors: {
+        primary: '#BF2A2A',
+
+        secondary: '#F2A922',
+
+        success: '#12A454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#ffffff',
+        title: '#363F5F',
+        text: '#8D8D8D',
+        background: '#F0F2F5',
+    },
+    fonts: {
+        // Usar fontes de Google Fonts com o expo fonts - 
+        // Não esquecer de importar no arquivo src/app.tsx 
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    }
+}
